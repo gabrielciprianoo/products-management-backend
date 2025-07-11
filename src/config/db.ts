218ce0b3e6bf77b9c15e188dfe05ef87522b2c06
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-11 13:45:55 -0600

    ✨ feat: implement Product model and configure Sequelize with TypeScript support

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,11 @@
+import Product from "../models/Product.model";
 import { RENDER_DATABASE_URL } from "./data";
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 
-const db = new Sequelize(RENDER_DATABASE_URL!);
+const db = new Sequelize(RENDER_DATABASE_URL!, {
+    models:  [ __dirname + '../models/**/*.ts' ]
+});
+
+db.addModels([Product])
 
 export default db;
