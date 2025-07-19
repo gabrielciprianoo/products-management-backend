aa19c09683a87fc8a7772275040a0f552aedce08
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-19 09:19:12 -0600

    ✨ feat: implement update product handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,10 +3,8 @@ import Product from "../models/Product.model";
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
-    
     const product = await Product.create(req.body);
     res.status(201).json({ data: product });
-
   } catch (error) {
     res.status(400).json({ error: error });
   }
@@ -14,10 +12,8 @@ export const createProduct = async (req: Request, res: Response) => {
 
 export const getProducts = async (req: Request, res: Response) => {
   try {
-    
     const products = await Product.findAll();
-    res.json({ data: products});
-
+    res.json({ data: products });
   } catch (error) {
     res.status(400).json({ error: error });
   }
@@ -25,15 +21,34 @@ export const getProducts = async (req: Request, res: Response) => {
 
 export const getProductById = async (req: Request, res: Response) => {
   try {
+    const { id } = req.params;
 
-    const { id } = req.params
-    
     const product = await Product.findByPk(id);
-    
-    if(!product){
-      res.status(404).json({ error: 'Producto no encontrado' });
-      return
+
+    if (!product) {
+      res.status(404).json({ error: "Producto no encontrado" });
+      return;
     }
+    res.json({ data: product });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      res.status(404).json({ error: "Producto no encontrado" });
+      return;
+    }
+
+    product.update(req.body);
+    product.save();
+
     res.json({ data: product });
     
   } catch (error) {
