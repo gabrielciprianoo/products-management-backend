5c25f8d68d2ba9c92ded8290d0930f3de0dc747a
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-20 15:19:22 -0600

    ♻️ refactor: improve test assertions for GET /api endpoint

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -4,8 +4,12 @@ import server from '../server';
 describe('GET /api', () => {
     test('should send back a json response', async () => {
         const response = await request(server).get('/api');
+
         expect(response.status).toBe(200);
         expect(response.headers['content-type']).toMatch(/json/);
         expect(response.body.msg).toBe('Desde API');
+
+        expect(response.status).not.toBe(404);
+        expect(response.body.msg).not.toBe('desde api');
     })
 })
