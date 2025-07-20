69380813054686b67237581d1d7e7a21c7879058
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-20 15:16:48 -0600

    ✨ feat: add initial test for GET /api endpoint

---
+++ src/__test__/server.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import server from '../server';
+
+describe('GET /api', () => {
+    test('should send back a json response', async () => {
+        const response = await request(server).get('/api');
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toMatch(/json/);
+        expect(response.body.msg).toBe('Desde API');
+    })
+})
