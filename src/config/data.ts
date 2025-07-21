210480ce1aaba8694a222b29b621bb4b8db0fd30
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-21 09:44:48 -0600

    ♻️ refactor: configure dotenv with debug and quiet options

--- src/config/data.ts
+++ src/config/data.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({
+  debug: false,
+  quiet: true 
+});
 
 export const RENDER_DATABASE_URL = process.env.RENDER_DATABASE_URL
 export const PORT = process.env.PORT || 4000
