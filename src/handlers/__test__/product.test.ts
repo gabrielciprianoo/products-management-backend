cf63ac8b205716a42d6832a02b875dea52acc65c
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-21 09:44:56 -0600

    ✨ feat: add tests for product creation and validation in POST /api/products

---
+++ src/handlers/__test__/product.test.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import server from "../../server";
+
+describe("POST /api/products", () => {
+
+  test("should validation product" , async () => {
+    const response = await request(server).post('/api/products').send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('errors');
+    expect(response.body.errors).toHaveLength(6);
+    
+    expect(response.status).not.toBe(201);
+    expect(response.body.errors).not.toHaveLength(2);
+
+  })
+
+  test("should create a new product", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "mouse testing",
+      price: 50,
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.status).not.toBe(400);
+    expect(response.status).not.toBe(404);
+    expect(response.status).not.toBe(200);
+    expect(response.body).toHaveProperty('data');
+    expect(response.body).not.toHaveProperty('errors');
+  });
+});
