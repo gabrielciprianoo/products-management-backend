c68c7f3c5a636b7896848cfa05557e644ea78a0a
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-21 09:52:52 -0600

    ♻️ refactor: improve formatting and consistency in product validation tests

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -2,18 +2,44 @@ import request from "supertest";
 import server from "../../server";
 
 describe("POST /api/products", () => {
-
-  test("should validation product" , async () => {
-    const response = await request(server).post('/api/products').send({});
+  test("should validation product", async () => {
+    const response = await request(server).post("/api/products").send({});
 
     expect(response.status).toBe(400);
-    expect(response.body).toHaveProperty('errors');
+    expect(response.body).toHaveProperty("errors");
     expect(response.body.errors).toHaveLength(6);
-    
+
     expect(response.status).not.toBe(201);
     expect(response.body.errors).not.toHaveLength(2);
+  });
 
-  })
+  test("should validate that price is greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "mouse",
+      price: 0,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+
+    expect(response.status).not.toBe(201);
+    expect(response.body.errors).not.toHaveLength(2);
+  });
+
+  test("should validate that price is a number", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "mouse",
+      price: "hola",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(2);
+
+    expect(response.status).not.toBe(201);
+    expect(response.body.errors).not.toHaveLength(3);
+  });
 
   test("should create a new product", async () => {
     const response = await request(server).post("/api/products").send({
@@ -25,7 +51,7 @@ describe("POST /api/products", () => {
     expect(response.status).not.toBe(400);
     expect(response.status).not.toBe(404);
     expect(response.status).not.toBe(200);
-    expect(response.body).toHaveProperty('data');
-    expect(response.body).not.toHaveProperty('errors');
+    expect(response.body).toHaveProperty("data");
+    expect(response.body).not.toHaveProperty("errors");
   });
 });
