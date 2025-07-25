e6bcffbc2b44cecdf9974d05bfb7874a0a47242e
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-24 18:53:14 -0600

    ✨ feat: implement custom CORS middleware for enhanced security

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./router";
 import db from "./config/db";
+import cors, { CorsOptions} from 'cors';
 
 async function conecctDB() {
   await db.authenticate();
@@ -8,6 +9,18 @@ async function conecctDB() {
 }
 
 const server = express();
+const corsOptions : CorsOptions = {
+  origin: (origin, callback) => {
+    if(origin !== process.env.FRONTEND_URL){
+      callback(new Error('Connection Denied'),false);
+      return;
+    }
+    callback(null, true);
+  }
+}
+
+server.use(cors(corsOptions));
+
 server.use(express.json());
 conecctDB();
 
